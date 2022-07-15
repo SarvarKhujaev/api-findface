9068c8bf0f363e88eb2c9b92ac06abb62c2a7ba7
Sarvar Khujaev <sarvar.khujaev@ssd.uz>
2022-07-15 19:41:04 +0500

    now findface works with cars and person ))

--- src/main/java/com/ssd/mvd/controller/RSocketConfigs.java
+++ src/main/java/com/ssd/mvd/controller/RSocketConfigs.java
@@ -1,23 +1,17 @@
 package com.ssd.mvd.controller;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class RSocketConfigs {
-    @Lazy
+    @Value( "${variables.LOAD_BALANCER}" )
+    private String host;
+    @Value( "${variables.PRODUCER_PORT}" )
+    private Integer port;
+
     @Bean
-    public WebClient webClient ( WebClient.Builder builder ) { return builder.build(); }
-
-    @Lazy
-    @Bean( name = "tablets" ) // connection to tablets service
-    RSocketRequester tablets ( RSocketRequester.Builder builder ) { return builder.tcp( "10.254.1.229", 6060 ); }
-
-    @Primary
-    @Bean( name = "findFaceForImages" ) // connection to trackers service
-    RSocketRequester connectToAssomidinService ( RSocketRequester.Builder builder ) { return builder.tcp( "10.254.1.2", 5055 ); }
+    RSocketRequester tablets ( RSocketRequester.Builder builder ) { return builder.tcp( this.host, this.port ); }
 }
--- src/main/java/com/ssd/mvd/controller/RequestController.java
+++ src/main/java/com/ssd/mvd/controller/RequestController.java
@@ -9,44 +9,39 @@ import com.ssd.mvd.database.Archive;
 import com.ssd.mvd.constants.Status;
 import com.ssd.mvd.component.FindFaceComponent;
 import com.ssd.mvd.database.CassandraDataControl;
-
 import com.ssd.mvd.entity.modelForCadastr.Person;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
-@RequestMapping( value = "/findFaceService/api/v1/" )
 public class RequestController {
     private final FindFaceComponent component;
 
     @MessageMapping ( value = "getPersonTotalData" )
-    public Mono< PsychologyCard > getPersonTotalData ( String base64url ) {
-        System.out.println( "Person Data" );
-        return base64url != null && base64url.length() > 0 ? this.component.getPapilonList( base64url )
-            .filter( value -> value.getResults() != null && value.getResults().size() > 0 )
-            .onErrorStop()
+    public Mono< PsychologyCard > getPersonTotalData ( String base64url ) { return base64url != null && base64url.length() > 0 ? this.component.getPapilonList( base64url )
+            .filter( value -> value.getResults() != null && value.getResults().size() > 0 ).onErrorStop()
             .map( value -> value.getResults().get( 0 ).getCountry().equals( "УЗБЕКИСТАН" ) ? SerDes.getSerDes().getPsychologyCard( value.getResults().get( 0 ).getPassport().split( " " )[0], value.getResults(), value.getViolationList() )
                     : new PsychologyCard( value ) ).onErrorStop() : Mono.empty(); }
 
+    @MessageMapping ( value = "getPersonTotalDataByPinfl" )
+    public Mono< PsychologyCard > getPersonTotalDataByPinfl ( String pinfl ) { return this.getPersonTotalData( SerDes.getSerDes().getImageByPnfl( pinfl ) ); }
+
     @MessageMapping ( value = "getPersonalCadastor" )
     public Mono< List< PsychologyCard > > getPersonalCadastor ( String id ) {
-        System.out.println( "Cadastor" );
         List< PsychologyCard > list = new ArrayList<>();
         List< Person > personList = SerDes.getSerDes().deserialize( id ).getPermanentRegistration();
         if ( personList != null && personList.size() > 0 ) Flux.fromStream( personList.stream() ).map( person -> this.getPersonTotalData( SerDes.getSerDes().getImageByPnfl( person.getPCitizen() ) ).subscribe( psychologyCard -> list.add( psychologyCard ) ) );
         return Mono.just( list ).delayElement( Duration.ofMillis( list.size() > 0 ? 300L * list.size() : 0L ) ); }
 
     @MessageMapping ( value = "getCarTotalData" )
-    public Mono< CarTotalData > getCarTotalData ( String platenumber ) {
-        System.out.println( "Person" );
-        return Archive.getInstance().getPreferenceItemMapForCar().containsKey( platenumber ) ? Mono.just( Archive.getInstance().getCarTotalData( platenumber ) ) :
+    public Mono< CarTotalData > getCarTotalData ( String platenumber ) { return Archive.getInstance().getPreferenceItemMapForCar().containsKey( platenumber ) ? Mono.just( Archive.getInstance().getCarTotalData( platenumber ) ) :
                 Mono.just( new CarTotalData() ).flatMap( carTotalData -> {
                     carTotalData.setDoverennostList( SerDes.getSerDes().getDoverennostList( platenumber ) );
                     carTotalData.setViolationsList( SerDes.getSerDes().getViolationList( platenumber ) );
