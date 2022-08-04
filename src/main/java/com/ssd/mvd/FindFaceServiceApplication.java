91c3730ff77bef8da7ac0626dc7fa8837a772816
Sarvar Khujaev <sarvar.khujaev@ssd.uz>
2022-08-04 15:58:36 +0500

    ModelForCar was updated

--- src/main/java/com/ssd/mvd/FindFaceServiceApplication.java
+++ src/main/java/com/ssd/mvd/FindFaceServiceApplication.java
@@ -10,7 +10,11 @@ public class FindFaceServiceApplication {
     public static ApplicationContext context;
 
     public static void main( String[] args ) {
-        new Thread( SerDes.getSerDes(), "serdes" ).start();
-        context = SpringApplication.run( FindFaceServiceApplication.class, args );
+        String data = "AA6088411_25.05.1998";
+        String[] strings = data.split( "_" );
+        System.out.println( strings[0] + " " + strings[1] );
+        System.out.println( SerDes.getSerDes().getPsychologyCard( SerDes.getSerDes().deserialize( strings[ 0 ], strings[ 1 ] ) ) );
+//        new Thread( SerDes.getSerDes(), "serdes" ).start();
+//        context = SpringApplication.run( FindFaceServiceApplication.class, args );
     }
 }
