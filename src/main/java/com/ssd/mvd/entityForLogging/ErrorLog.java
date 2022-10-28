96ba8afc559fc352ec383f2d20c4cbe50d632a27
Sarvar Khujaev <sarvar.khujaev@ssd.uz>
2022-10-28 11:34:48 +0500

    Admin Panel with logging was added

--- src/main/java/com/ssd/mvd/entityForLogging/ErrorLog.java
+++ src/main/java/com/ssd/mvd/entityForLogging/ErrorLog.java
@@ -1,15 +1,13 @@
 package com.ssd.mvd.entityForLogging;
 
-import java.util.List;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ErrorLog {
-    private String url;
+    private Long createdAt;
+    private String errorMessage;
     private String integratedService;
     private String integratedServiceApiDescription;
-
-    private List< Item > itemList;
 }
